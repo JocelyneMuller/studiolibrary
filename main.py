0635ee786ca30e700a10a20d95b099ec16699b13
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2024-12-23 21:58:50 +0100

    code main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from src.views.book_view import BookView
+from PyQt6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    view = BookView()
+    view.show()
+
+    sys.exit(app.exec_()) 
