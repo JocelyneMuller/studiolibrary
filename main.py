41e05b90ae5c0c760b9d1a15623e5cac7ac1bd80
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2024-12-24 08:59:24 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from src.views.book_view import BookView
-from PyQt6.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
